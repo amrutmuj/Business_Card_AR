92a1e3bc4318ad5498ea9c16aa6eb346efe7d46a
Amrut <85221126+amrutmuj@users.noreply.github.com>
2021-11-14 23:12:32 +0530

    Assets Commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    // This script help to access the buttons of Social Media
+
+    public void button_click_LinkedIn()
+    {
+        Application.OpenURL("#");  //Replace # with the URL of your LinkedIn Profile
+    }
+    public void button_click_Snap()
+    {
+        Application.OpenURL("#");  //Replace # with the URL of your Snapchat Profile
+    }
+    public void button_click_Youtube()
+    {
+        Application.OpenURL("#");  //Replace # with the URL of your Youtube Profile
+    }
+    public void button_click_Instagram()
+    {
+        Application.OpenURL("#");  //Replace # with the URL of your Instagram Profile
+    }
+
+    // Now attach this file to Social Media (Parent) Object. 
+    // Further add them to the respective OnClick().
+    
+}
